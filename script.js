a596ab99c1a337393ce932cd2a4bcbe4ad1ee7d3
Dylan Campos <es.dylanfreire@gmail.com>
2024-08-15 20:50:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const starsContainer = document.getElementById('stars-container');
+const message = document.getElementById('message');
+const numStars = 10;
+let clickedStars = 0;
+
+function createStar() {
+    const star = document.createElement('div');
+    star.classList.add('star');
+
+    // Definindo posições aleatórias
+    const randomX = Math.random() * 90; // Para garantir que a estrela fique dentro da tela
+    const randomY = Math.random() * 90;
+
+    star.style.position = 'absolute';
+    star.style.left = `${randomX}vw`;
+    star.style.top = `${randomY}vh`;
+
+    star.addEventListener('click', () => {
+        star.classList.add('hidden');
+        clickedStars++;
+        if (clickedStars === numStars) {
+            showInvitation();
+        }
+    });
+
+    starsContainer.appendChild(star);
+}
+
+function showInvitation() {
+    message.textContent = "Parabéns Fabiana! Você encontrou todas as estrelas e ganhou um encontro comigo. Uhuuuul!!! Volte agora para o direct do instagram para podermos marcar.";
+}
+
+// Criar estrelas
+for (let i = 0; i < numStars; i++) {
+    createStar();
+}
